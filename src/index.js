b23ce5c1b8005ce005f050235537ca801fe82670
Thanat <bztzn512@gmail.com>
2022-04-15 13:33:42 -0400

    changed

--- src/index.js
+++ src/index.js
@@ -3,11 +3,7 @@ import ReactDOM from "react-dom/client";
 import "normalize.css";
 import "./index.css";
 import App from "./App";
-<<<<<<< HEAD
-import { AppProvider } from "./context/AppContext";
-=======
 import { AppProvider } from "./context/appContext";
->>>>>>> 103911dbfb12a9c2ac7f0a06ee9df531e605be41
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
